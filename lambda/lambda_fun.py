be4cbfdedd61faec7b3b27826bba5dc8c3a52b21
Munir Nawaz <munir.nawaz@gmail.com>
2024-11-21 10:58:57 +0100

    comments added

--- lambda/lambda_fun.py
+++ lambda/lambda_fun.py
@@ -2,7 +2,7 @@ import json
 def lamda_handler(event, context):
     return {
         "statuscode" : 200,
-        "body" : json.dumps("Hellow CICD Lambda from vscode5")
+        "body" : json.dumps("Hellow CICD Lambda from vscode6")
     }
     
     
