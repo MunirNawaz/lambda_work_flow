ea669cd62a4e57a90be2f6ce7185c152a7008a4e
Munir Nawaz <munir.nawaz@gmail.com>
2024-11-21 11:40:52 +0100

    test workflow

--- lambda/lambda_fun.py
+++ lambda/lambda_fun.py
@@ -2,7 +2,7 @@ import json
 def lamda_handler(event, context):
     return {
         "statuscode" : 200,
-        "body" : json.dumps("Hellow Lambda from vs code3")
+        "body" : json.dumps("Hellow Lambda from vs code4")
     }
     
     
