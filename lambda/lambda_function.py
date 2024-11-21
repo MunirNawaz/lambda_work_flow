9683a7fa550c3ea0c0a6ee5afec63958f8b7afac
Munir Nawaz <munir.nawaz@gmail.com>
2024-11-21 12:20:40 +0100

    changed the name to lambda_function.py

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,7 +2,7 @@ import json
 def lamda_handler(event, context):
     return {
         "statuscode" : 200,
-        "body" : json.dumps("Hellow Lambda from vscode for my-lambda_test")
+        "body" : json.dumps("Hellow Lambda from vscode-1 for my-lambda_test")
     }
     
     
