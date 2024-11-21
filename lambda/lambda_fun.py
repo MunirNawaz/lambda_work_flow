8e96f3885ecd3739091c3a5255f1f59c90d66a21
Munir Nawaz <munir.nawaz@gmail.com>
2024-11-21 11:34:23 +0100

    commit

--- lambda/lambda_fun.py
+++ lambda/lambda_fun.py
@@ -2,7 +2,7 @@ import json
 def lamda_handler(event, context):
     return {
         "statuscode" : 200,
-        "body" : json.dumps("Hellow Lambda from vscode8")
+        "body" : json.dumps("Hellow Lambda from vs code")
     }
     
     
